a8c77210cda5fdbbcf47bba4f0af253e4624e019
Ranjesh Prabhat <rsprabhat2000@gmail.com>
2021-11-14 19:40:08 +0530

    14th commit:CIY - v1 released

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 
 const Categories = () => {
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [])
 
     return (
         <>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import Carousel from './Carousel'
 import Randomdish from './Randomdish'
 
 const Home = () => {
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [])
+
     return (
         <>
             <div className="container-fluid">
-                <Carousel/>
-                <hr className="hr-1"/>
-                <Randomdish/>
+                <Carousel />
+                <hr className="hr-1" />
+                <Randomdish />
             </div>
         </>
     )
